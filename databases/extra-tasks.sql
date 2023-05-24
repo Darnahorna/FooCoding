cf5e30d1660a7d98a27cc868d3706d22c040d567
Daria Nahorna <46358667+Darnahorna@users.noreply.github.com>
2023-05-24 17:16:54 +0300

    Update extra-tasks.sql

--- databases/extra-tasks.sql
+++ databases/extra-tasks.sql
@@ -1,5 +1,5 @@
 #List of the names of the countries that has more than 10 cities, and a total population of the cities listed (not the country population) of more than 50 million
-SELECT country.Name, country.Population, count(city.Name), sum(city.Population) as yy FROM new_world.country INNER JOIN new_world.city ON country.Code=city.CountryCode GROUP BY country.Name HAVING count(city.Name)>10 AND sum(city.Population)>50000000;
+SELECT country.Name FROM new_world.country INNER JOIN new_world.city ON country.Code=city.CountryCode GROUP BY country.Name HAVING count(city.Name)>10 AND sum(city.Population)>50000000;
 
 #List the cities from those countries with >50M pop in the listed cities, where the city population is > 5M  
 SELECT city.Name, city.Population, country.Name, country.cityPop FROM 
