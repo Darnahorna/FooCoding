b6f3c4b5c2212021a12c40e2a0ac490b60e54c88
Daria Nahorna <46358667+Darnahorna@users.noreply.github.com>
2023-06-01 18:40:22 +0300

    Delete javasript-part-two directory

--- javasript-part-two/week02/doubleOddNumbers.js
+++
@@ -1,7 +0,0 @@
-function doubleOddNumbers(numbers) {
-  return numbers
-    .filter((number) => number % 2 !== 0)
-    .map((number) => number * 2);
-}
-
-module.exports = doubleOddNumbers;
--- javasript-part-two/week02/doubleOddNumbers.test.js
+++
@@ -1,7 +0,0 @@
-const doubleOddNumbers = require("./doubleOddNumbers");
-
-test("double odd numbers in array", () => {
-  expect(doubleOddNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9])).toStrictEqual([
-    2, 6, 10, 14, 18,
-  ]);
-});
